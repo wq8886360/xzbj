67f848716194cf48494fef1246735a1de466536a
葛佳阳 <18358205737@163.com>
2018-03-07 10:44:25 +0800

    新增首页入口、iview模块

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+
+//modules
+import iView from "iview"; //iview模块
 
 //styles
-import './assets/styles/reset.css'; //初始化默认样式
+import "./assets/styles/reset.css"; //初始化默认样式
+import "iview/dist/styles/iview.css"; //iview样式
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: "#app",
+    router,
+    components: { App },
+    template: "<App/>"
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Test from '../view/test.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Index from "../view/index.vue"; //首页
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      component: Test
-    }
-  ]
-})
+    routes: [
+        {
+            path: "/",
+            component: Index,
+            meta: { title: "首页" }
+        }
+    ]
+});
