9c669fb3f99be3a932130a37b1ed7e6e2fd1b941
潘佳佳 <1556069939@qq.com>
2018-03-07 13:40:35 +0800

    添加图片

--- src/view/index.vue
+++ src/view/index.vue
@@ -2,8 +2,17 @@
     <div class="index">
         <div class="head">
             <div class="head-logo">
-                <img src="../assets/logo.png" alt="">
+                <img src="../assets/img/logo.png" alt="">
+            </div>
+            <div class="head-search">
+                <input type="text" placeholder="请输入">
             </div>
         </div>
     </div>
 </template>
+<script>
+
+</script>
+<style lang="less">
+@import "../assets/styles/index.less";
+</style>
