fd92637f03b8568c192b57f92bb72a4a3da4bc55
葛佳阳 <18358205737@163.com>
2018-03-08 17:47:11 +0800

    Change

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
     <div id="app">
         <vheader/>
         <router-view/>
+        <vfooter/>
     </div>
 </template>
 
 <script>
 import Vheader from "./components/header.vue";
+import Vfooter from "./components/footer.vue";
 export default {
     name: "App",
     components: {
-        Vheader
+        Vheader,
+        Vfooter
     }
 };
 </script>
---
+++ src/components/footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="footer">
+         <div class="list">
+            <a href="">卓路官网</a>
+            <a href="">客户端下载</a>
+            <a href="">企业招聘</a>
+         </div>
+         <div class="record">Copyright© 杭州卓路科技有限公司 浙ICP备17053397号-1</div>
+        <div><img class="ghs" src="../assets/img/ghs.png" alt="">浙公网安备33010802008693号</div>
+    </div>
+</template>
+<style lang="less">
+.footer {
+    padding: 34px 0 40px;
+    background: #8898ab;
+    text-align: center;
+    font-size: 14px;
+    color: #fff;
+    .list {
+        a {
+            margin: 0 20px;
+        }
+    }
+    .record {
+        margin: 20px 0 2px;
+    }
+    .ghs {
+        width: 20px;
+        height: 20px;
+        vertical-align: middle;
+        margin-right: 14px;
+    }
+}
+</style>
