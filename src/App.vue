d345cd47c95dcf6a8c6c46b2d9f3877582a74aa7
葛佳阳 <18358205737@163.com>
2018-03-08 09:40:36 +0800

    app.vue添加header组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
     <div id="app">
+        <vheader/>
         <router-view/>
     </div>
 </template>
 
 <script>
+import Vheader from "./components/header.vue";
 export default {
     name: "App",
-    components: {}
+    components: {
+        Vheader
+    }
 };
 </script>
 
