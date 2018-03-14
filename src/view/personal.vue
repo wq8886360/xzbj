3c472d537c025e752cf36fde2015f30b26b24ef9
潘佳佳 <1556069939@qq.com>
2018-03-14 16:58:40 +0800

    logo图片大小修改

---
+++ src/view/personal.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="personal">
+        <search/>
+        <!-- 个人中心左边 -->
+        <div class="head-left">
+            <p>个人中心</p>
+            <div class="head-bottom">
+                <ul>
+                    <li>我的订单</li>
+                    <li>地址管理</li>
+                    <li>个人信息</li>
+                </ul>
+            </div>
+        </div>
+        <!-- 个人中心右边 -->
+
+    </div>
+
+</template>
+<script>
+import Search from "../components/search.vue";
+import { Icon } from "iview";
+export default {
+    components: {
+        Search
+    }
+}
+
+
+</script>
+<style lang="less">
+@import "../assets/styles/personal.less";
+</style>
