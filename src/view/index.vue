a4cecf6eadad51d8247a35c8c8b1951e8cb3e604
潘佳佳 <1556069939@qq.com>
2018-03-16 14:23:21 +0800

    个人中心

--- src/view/index.vue
+++ src/view/index.vue
@@ -46,6 +46,7 @@
                         </li>
                     </ul>
                 </div>
+
             </div>
             <div class="menu">
                 <ul>
@@ -58,82 +59,97 @@
                 </ul>
             </div>
         </div>
-        <!-- 轮播图 -->
-        <div class="ivue-banner">
-            <Carousel autoplay arrow="never">
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../assets/img/banner.jpg" alt=""></div>
-                </Carousel-item>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../assets/img/banner.jpg" alt=""></div>
-                </Carousel-item>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../assets/img/banner.jpg" alt=""></div>
-                </Carousel-item>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../assets/img/banner.jpg" alt=""></div>
-                </Carousel-item>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../assets/img/banner.jpg" alt=""></div>
-                </Carousel-item>
-            </Carousel>
+        <div class="effective" v-if="true">
+            <!-- 轮播图 -->
+            <div class="ivue-banner">
+                <Carousel autoplay arrow="never">
+                    <Carousel-item>
+                        <div class="demo-carousel"><img src="../assets/img/banner.jpg" alt=""></div>
+                    </Carousel-item>
+                    <Carousel-item>
+                        <div class="demo-carousel"><img src="../assets/img/banner.jpg" alt=""></div>
+                    </Carousel-item>
+                    <Carousel-item>
+                        <div class="demo-carousel"><img src="../assets/img/banner.jpg" alt=""></div>
+                    </Carousel-item>
+                    <Carousel-item>
+                        <div class="demo-carousel"><img src="../assets/img/banner.jpg" alt=""></div>
+                    </Carousel-item>
+                    <Carousel-item>
+                        <div class="demo-carousel"><img src="../assets/img/banner.jpg" alt=""></div>
+                    </Carousel-item>
+                </Carousel>
 
-        </div>
-        <!-- 家具商品 -->
-        <div class="good-box">
-            <div class="goods">
-                <div class="goods-title">
-                    <span class="goods-headline">家居商品</span>
-                    <div class="goods-more">
-                        <span class="goods-added">更多</span>
-                        <Icon class="goods-added-ios" type="ios-arrow-right"></Icon>
-                        <Icon class="goods-added-ios" type="ios-arrow-right"></Icon>
-                    </div>
-                </div>
-                <div class="goods-list">
-                    <div class="good-list-left">
-                        <img src="../assets/img/houseHold.jpg" alt="">
-                        <div class="goods-list-title">
-                            <span>家居</span>
-                            <p>高品质生活的首选</p>
+            </div>
+            <!-- 家具商品 -->
+            <div class="good-box">
+                <div class="goods">
+                    <div class="goods-title">
+                        <span class="goods-headline">家居商品</span>
+                        <div class="goods-more">
+                            <span class="goods-added">更多</span>
+                            <Icon class="goods-added-ios" type="ios-arrow-right"></Icon>
+                            <Icon class="goods-added-ios" type="ios-arrow-right"></Icon>
                         </div>
                     </div>
-                    <div class="good-list-right">
-                        <div class="good-picture" v-for="(item,index) in 8" :key="index">
-                            <img src="../assets/img/bathroom.jpg" alt="">
-                            <div class="good-mask">
-                                <p>家居·床</p>
+                    <div class="goods-list">
+                        <div class="good-list-left">
+                            <img src="../assets/img/houseHold.jpg" alt="">
+                            <div class="goods-list-title">
+                                <span>家居</span>
+                                <p>高品质生活的首选</p>
+                            </div>
+                        </div>
+                        <div class="good-list-right">
+                            <div class="good-picture" v-for="(item,index) in 8" :key="index">
+                                <img src="../assets/img/bathroom.jpg" alt="">
+                                <div class="good-mask">
+                                    <p>家居·床</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 建材商品 -->
+                <div class="goods">
+                    <div class="goods-title">
+                        <span class="goods-headline">建材商品</span>
+                        <div class="goods-more">
+                            <span class="goods-added">更多</span>
+                            <Icon class="goods-added-ios" type="ios-arrow-right"></Icon>
+                            <Icon class="goods-added-ios" type="ios-arrow-right"></Icon>
+                        </div>
+                    </div>
+                    <div class="goods-list">
+                        <div class="good-list-left">
+                            <img src="../assets/img/houseHold.jpg" alt="">
+                            <div class="goods-list-title">
+                                <span>建材</span>
+                                <p>绿色、健康、环保</p>
+                            </div>
+                        </div>
+                        <div class="good-list-right">
+                            <div class="good-picture" v-for="(item,index) in 8" :key="index">
+                                <img src="../assets/img/bathroom.jpg" alt="">
+                                <div class="good-mask">
+                                    <p>绿色、健康、环保</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- 建材商品 -->
-            <div class="goods">
-                <div class="goods-title">
-                    <span class="goods-headline">建材商品</span>
-                    <div class="goods-more">
-                        <span class="goods-added">更多</span>
-                        <Icon class="goods-added-ios" type="ios-arrow-right"></Icon>
-                        <Icon class="goods-added-ios" type="ios-arrow-right"></Icon>
-                    </div>
+        </div>
+
+        <div class="null" v-if="false">
+            <!-- 首页搜索为空 -->
+            <div class="empty">
+                <div class="empty-image">
+                    <img src="../assets/img/commoditySearch.png" alt="">
                 </div>
-                <div class="goods-list">
-                    <div class="good-list-left">
-                        <img src="../assets/img/houseHold.jpg" alt="">
-                        <div class="goods-list-title">
-                            <span>建材</span>
-                            <p>绿色、健康、环保</p>
-                        </div>
-                    </div>
-                    <div class="good-list-right">
-                        <div class="good-picture" v-for="(item,index) in 8" :key="index">
-                            <img src="../assets/img/bathroom.jpg" alt="">
-                            <div class="good-mask">
-                                <p>绿色、健康、环保</p>
-                            </div>
-                        </div>
-                    </div>
+                <div class="empty-cue">
+                    <p>亲爱的，很抱歉，没有找到与
+                        <span>“烟灰色大理石”</span>相关的商品</p>
                 </div>
             </div>
         </div>
--- src/view/personal.vue
+++ src/view/personal.vue
@@ -1,18 +1,87 @@
 <template>
     <div class="personal">
         <search/>
-        <!-- 个人中心左边 -->
-        <div class="head-left">
-            <p>个人中心</p>
-            <div class="head-bottom">
-                <ul>
-                    <li>我的订单</li>
-                    <li>地址管理</li>
-                    <li>个人信息</li>
-                </ul>
+        <div class="head-content">
+            <!-- 个人中心左边 -->
+            <div class="head-left">
+                <p>个人中心</p>
+                <div class="head-bottom">
+                    <ul>
+                        <li>我的订单</li>
+                        <li>地址管理</li>
+                        <li>个人信息</li>
+                    </ul>
+                </div>
             </div>
+            <!-- 个人中心右边 -->
+            <div class="head-right">
+                <div class="head-right-title">
+                    <ul>
+                        <li>全部订单</li>
+                        <li>待付款
+                            <span>0</span>
+                        </li>
+                        <li>待发货
+                            <span>2</span>
+                        </li>
+                        <li>待收货
+                            <span>3</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="heading">
+                    <ul>
+                        <li class="goods">商品</li>
+                        <li class="price">单价(元)</li>
+                        <li class="numeber">数量</li>
+                        <li class="payment">实付款(元)</li>
+                        <li class="state">交易状态</li>
+                        <li class="operate">操作</li>
+                    </ul>
+                </div>
+                <!-- 订单 -->
+                <div class="order">
+                    <div class="order-title">
+                        <span>订单编号：20170220199165478962</span>
+                        <span>2018-3-12 13:56</span>
+                        <span>店铺：坚哥一号店</span>
+                    </div>
+                    <div class="order-box">
+                        <div class="order-left">
+                            <ul>
+                                <li>
+                                    <img src="../assets/img/bathroom.jpg" alt="">
+                                </li>
+                                <li class="spec">
+                                    <span>西门子开关面板西门子开关面板</span>
+                                    <p>800*800mm</p>
+                                </li>
+                                <li class="rate">
+                                    <p>24.00</p>
+                                </li>
+                                <li class="amount">
+                                    <p>5</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="order-right">
+                            <ul>
+                                <li class="cost">
+                                    <span>¥120.00</span>
+                                    <p>（包邮）</p>
+                                </li>
+                                <li class="details">
+                                    <p>买家已付款</p>
+                                    <p>订单详情</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
-        <!-- 个人中心右边 -->
 
     </div>
 
