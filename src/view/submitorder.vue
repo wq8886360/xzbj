cebc90cc84eed89ed2a52a014e2d8f2f05b1c2b8
葛佳阳 <18358205737@163.com>
2018-03-21 17:36:08 +0800

    change

---
+++ src/view/submitorder.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="submitorder">
+        <search></search>
+        <div class="step">
+            <Steps :current="current">
+                <Step title="提交订单"></Step>
+                <Step title="支付"></Step>
+                <Step title="完成订单"></Step>
+            </Steps>
+        </div>
+        <div class="address">
+            <div class="address-list">
+                <div class="item active"></div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { Steps, Step } from "iview";
+import Search from "../components/search.vue";
+export default {
+    components: {
+        Search,
+        Steps,
+        Step
+    },
+    data() {
+        return {
+            current: 0
+        };
+    }
+};
+</script>
+<style lang="less">
+@import "../assets/styles/submitorder.less";
+</style>
+
+
