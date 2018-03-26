dced5f93bf1e28e2d29a4dc24ade5421cf8c7ddf
葛佳阳 <18358205737@163.com>
2018-03-26 13:29:01 +0800

    Change

---
+++ src/view/agreement.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="agreement">
+
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {};
+    }
+};
+</script>
+<style>
+@import "../assets/styles/agreement.less";
+</style>
+
+
